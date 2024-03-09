0292288b855a65ff23974d1b3911992e4a67ba76
dgb1106 <duonggiabao1106@gmail.com>
2024-03-09 20:18:44 +0700

    commit

---
+++ bai4_mouse_event/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "defs.h"
+#include "graphics.h"
+
+using namespace std;
+
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while (true) {
+        if (SDL_PollEvent(&e) != 0 && (e.type == SDL_KEYDOWN || e.type == SDL_QUIT)) {
+            return;
+        }
+        SDL_Delay(100);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    Graphics graphics;
+    graphics.init();
+
+    SDL_Rect rect;
+    rect.x = 100;
+    rect.y = 100;
+    rect.h = 100;
+    rect.w = 100;
+    SDL_SetRenderDrawColor(graphics.renderer, 255, 255, 255, 0);
+    SDL_RenderFillRect(graphics.renderer, &rect);
+    SDL_RenderPresent(graphics.renderer);
+
+    SDL_Event event;
+    int x, y;
+    while (true) {
+        SDL_GetMouseState(&x, &y);
+
+        /*if (x > 100 && y > 100 && x < 200 && y < 200) cerr << "In" << endl;
+        else cerr << "Out" << endl;*/
+
+        SDL_PollEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT:
+                exit(0);
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                cerr << "Down at (" << x  << "," << y << ")" << endl;
+                break;
+            case SDL_MOUSEBUTTONUP:
+                cerr << "Up at (" << x  << "," << y << ")" << endl;
+                break;
+        }
+
+        SDL_Delay(100);
+    }
+
+    graphics.quit();
+
+    return 0;
+}
