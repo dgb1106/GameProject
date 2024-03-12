64f45e7fe5a09bdfd642c1d7fafbfeb90201c143
dgb1106 <duonggiabao1106@gmail.com>
2024-03-12 22:58:24 +0700

    keyboard events

---
+++ bai6_keyboard_event/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "defs.h"
+#include "graphics.h"
+
+using namespace std;
+
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while (true) {
+        if (SDL_PollEvent(&e) != 0 && (e.type == SDL_KEYDOWN || e.type == SDL_QUIT)) {
+            return;
+        }
+        SDL_Delay(100);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    Graphics graphics;
+    graphics.init();
+
+    bool quit = false;
+
+    SDL_Event event;
+
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) quit = true;
+        }
+
+        const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+
+        if (currentKeyStates[SDL_SCANCODE_UP]) cerr << " Up";
+        if (currentKeyStates[SDL_SCANCODE_DOWN]) cerr << " Down";
+        if (currentKeyStates[SDL_SCANCODE_LEFT]) cerr << " Left";
+        if (currentKeyStates[SDL_SCANCODE_RIGHT]) cerr << " Right";
+
+        cerr << "." << endl;
+
+        SDL_Delay(100);
+    }
+
+    graphics.quit();
+
+    return 0;
+}
