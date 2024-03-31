6e0693171b5ca2f3d3c68c213e6d46187d26ad18
dgb1106 <duonggiabao1106@gmail.com>
2024-03-31 21:41:03 +0700

    background and ball

--- MyGame/defs.h
+++ MyGame/defs.h
@@ -5,7 +5,7 @@ const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 #define WINDOW_TITLE "Game"
 
-#define BACKGROUND_IMG "assets\\bg.png"
+#define BACKGROUND_IMG "assets\\background.png"
 #define BALL_IMG "assets\\ball.png"
 
 #endif // _DEFS__H
